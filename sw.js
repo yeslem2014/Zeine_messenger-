c86ed63c7089a2557fb936c69ee2ca0a31770781
yeslem2014 <yeslem2014@gmail.com>
2026-06-10 08:19:25 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'zeine-messenger-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/inbox.html',
+  '/style.css',
+  '/script.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
